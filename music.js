9b91dae81b01f0bcca77f33230e26e0a771c335c
_Eckermania <127853724+Eckerman-ia@users.noreply.github.com>
2023-12-06 16:13:39 -0500

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,5 @@
 var audioElement0 = document.createElement('audio');
-audioElement0.setAttribute('src', 'music/spoons.mp3');
+audioElement0.setAttribute('src', 'music.mp3');
 audioElement0.setAttribute('autoplay', 'autoplay');
 audioElement0.play(); 
 audioElement0.addEventListener('ended', function() {
